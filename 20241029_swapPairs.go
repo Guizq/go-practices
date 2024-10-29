a51b82c76c6bfd0c230a1910403c003c84004691
Guizq <124554562+Guizq@users.noreply.github.com>
2024-10-29 15:23:44 +0800

    Add files via upload

---
+++ 20241029_swapPairs.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func intersection(num1 []int, num2 []int) []int {
+	set := make(map[int]struct{}, 0)
+	res := make([]int, 0)
+	for _, v := range num1 {
+		if _, ok := set[v]; !ok {
+			set[v] = struct{}{}
+		}
+	}
+	for _, v := range num2 {
+		if _, ok := set[v]; ok {
+			res = append(res, v)
+			delete(set, v)
+		}
+	}
+	return res
+}
+
+func main() {
+	nums1 := []int{1, 2, 2, 3}
+	nums2 := []int{2, 2}
+	fmt.Println(intersection(nums1, nums2))
+}
